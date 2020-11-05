336d766c8fb4c5f196b4b67b785e01a2b850e732
Kike Sanchez <kikin1601@hotmail.com>
2020-11-05 15:51:27 -0500

    :rocket: primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PoliticasComponent} from './politicas/politicas.component';
+import {HomeComponent} from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'politicas', component: PoliticasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoliticasComponent } from './politicas/politicas.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PoliticasComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
